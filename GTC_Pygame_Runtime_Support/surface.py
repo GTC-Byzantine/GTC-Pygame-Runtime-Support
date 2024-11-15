389fde2cdc11bdd01d08d0b489a50546ca76eafe
GTC-Byzantine <118645189+GTC-Byzantine@users.noreply.github.com>
2024-11-15 14:33:53 +0800

    Add files via upload

--- GTC_Pygame_Runtime_Support/surface.py
+++ GTC_Pygame_Runtime_Support/surface.py
@@ -1,4 +1,5 @@
 import pygame
+from GTC_Pygame_Runtime_Support.basic_class import BasicChecker
 
 class CommonSurface:
     def __init__(self, size, pos, screen):
@@ -6,6 +7,30 @@ class CommonSurface:
         self.pos = pos
         self.surface = pygame.Surface(size)
         self.screen = screen
-        self.checkers = []
-    def add_checker(self, checker, motion):
-        
+        self.checkers = {}
+    def add_checker_group(self, group_name, motion, checker_type='and'):
+        """
+        :param group_name:
+        :type group_name:               str
+        :param motion:
+        :type motion:                   function
+        :param checker_type:
+        :type checker_type:             str
+        :return:                        none
+        """
+        self.checkers[group_name] = {'checkers': [], 'motion': motion, 'type': checker_type}
+    def add_checker(self, group_name, checker):
+        """
+        :param checker:
+        :type checker:                  BasicChecker
+        :param group_name:
+        :type group_name:               str
+        :return:
+        """
+        self.checkers[group_name]['checkers'].append(checker)
+
+    def operate(self, mouse_pos, mouse_click):
+        for groups in self.checkers:
+            for checker in self.checkers[groups]['checkers']:
+                pass
+
