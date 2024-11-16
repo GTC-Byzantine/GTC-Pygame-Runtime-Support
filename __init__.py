98ee0d11f063636c3f534b35925c49430ed9c988
GTC-Byzantine <118645189+GTC-Byzantine@users.noreply.github.com>
2024-11-16 20:39:12 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-
-import GTC_Pygame_Runtime_Support.basic_class
-import GTC_Pygame_Runtime_Support.button
-import GTC_Pygame_Runtime_Support.page
-import GTC_Pygame_Runtime_Support.progress_bar
-import GTC_Pygame_Runtime_Support.surface
